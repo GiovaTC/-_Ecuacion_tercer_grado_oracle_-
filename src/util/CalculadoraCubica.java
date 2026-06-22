b91f7e10eb45923f0cf5edf01752687d88447a9e
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-22 10:24:53 -0500

    commit - CalculadoraCubica .

--- src/util/CalculadoraCubica.java
+++ src/util/CalculadoraCubica.java
@@ -3,14 +3,30 @@ package util;
 import modelo.Ecuacion;
 import modelo.PuntoGrafica;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculadoraCubica {
+
     public static Ecuacion calcularEcuacion(double a, double b, double c, double d) {
-        return null;
+        Ecuacion e  = new Ecuacion();
+        e.setA(a); e.setB(b); e.setC(c); e.setD(d);
+        e.setEcuacion(String.format("f(x)=%.2fx³+%.2fx²+%.2fx+%.2f", a, b, c, d));
+        e.setRaices("Raices calculadas aqui"); // simplificado .
+        return e;
     }
 
-    public static List<PuntoGrafica> generarPuntos(double a, double b, double c, double d, int id) {
-        return null;
-    }
+    public static List<PuntoGrafica> generarPuntos(double a, double b, double c, double d, int idEcuacion) {
+        List<PuntoGrafica> puntos = new ArrayList<>();
+
+        for (double x = -10; x <= 10; x += 0.05) {
+            double y = a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d;
+            PuntoGrafica p = new PuntoGrafica();
+            p.setIdEcuacion(idEcuacion);
+            p.setX(x);
+            p.setY(y);
+            puntos.add(p);
+        }
+        return puntos;
+    }   
 }
