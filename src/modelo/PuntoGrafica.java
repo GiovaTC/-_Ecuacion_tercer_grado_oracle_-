322eb8398429f4c4dc1792136a0493a1e382f9b1
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-22 09:58:59 -0500

    commit - PuntoGrafica / .

--- src/modelo/PuntoGrafica.java
+++ src/modelo/PuntoGrafica.java
@@ -1,4 +1,41 @@
 package modelo;
 
 public class PuntoGrafica {
+
+    private int id;
+    private int idEcuacion;
+    private double x;
+    private double y;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEcuacion() {
+        return idEcuacion;
+    }
+
+    public void setIdEcuacion(int idEcuacion) {
+        this.idEcuacion = idEcuacion;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }   
 }
